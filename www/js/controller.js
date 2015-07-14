272bf96b960d9e6e6cc31479abdfa488226b7792
gurinder <gurinder@headerlabs.com>
2015-07-14 19:02:26 +0530

    login for facebook

--- www/js/controller.js
+++ www/js/controller.js
@@ -2,11 +2,22 @@
 ionicFirebase.controller("LoginCtrl", function ($scope) {
     $scope.loginGithub = function(authMethod) {
         var ref = new Firebase("https://gurinder.firebaseio.com");
-        ref.authWithOAuthPopup("github", function(error, authData) {
+        ref.authWithOAuthPopup("github", function(error, authDataGithub) {
             if (error) {
                 console.log("Login Failed!", error);
             } else {
-                console.log("Authenticated successfully with payload:", authData);
+                console.log("Authenticated successfully with payload:", authDataGithub);
+            }
+        });
+    };
+
+    $scope.loginfacebook = function(authMethod) {
+        var ref = new Firebase("https://gurinder.firebaseio.com");
+        ref.authWithOAuthPopup("facebook", function(error, authDataFacebook) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authDataFacebook);
             }
         });
     };
